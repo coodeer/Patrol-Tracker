3d609e27fff6fc8c5c401f7b98b84c286cc386ee
Gaston Moreno <gaston.moreno@globallogic.com>
2014-09-25 19:22:50 -0300

    Add history.swf

--- Frontend/app/scripts/controllers.js
+++ Frontend/app/scripts/controllers.js
@@ -8,6 +8,10 @@ define('controllers',['services'], function(services){
       scope.subscribeToViewport = function subscribeToViewport(data, callback){
         return service.subscribeToViewport(data, callback);
       };
+
+      scope.sendNotification = function sendNotification(){
+          window.sendNotification({ _id: '9890ad8sf90a8', name: 'test', value: 90});
+      };
     }])
     .controller('ZoneCtrl',['$scope','trackableService', function(scope, service){
         console.log(service.getAll());
